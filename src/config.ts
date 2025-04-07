3152c4c41498ad6000b76bd99c7cfec699674ec6
Blooshiez <weesnowflakes@gmail.com>
2025-04-07 14:57:13 -0400

    Update config.ts
    minor changes for testing

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,8 @@ import type {
 import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
-  title: 'Fuwari',
-  subtitle: 'Demo Site',
+  title: 'yumeyume',
+  subtitle: 'Taso Site',
   lang: 'en',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -63,9 +63,9 @@ export const profileConfig: ProfileConfig = {
       url: 'https://twitter.com',
     },
     {
-      name: 'Steam',
+      name: 'QooApp',
       icon: 'fa6-brands:steam',
-      url: 'https://store.steampowered.com',
+      url: 'https://m-apps.qoo-app.com/en-US/app/74756',
     },
     {
       name: 'GitHub',
