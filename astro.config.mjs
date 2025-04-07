dcd7a1918fcfe725427ad818bc4c587a4516f02f
Blooshiez <weesnowflakes@gmail.com>
2025-04-07 15:08:39 -0400

    Update astro.config.mjs
    Changes to site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://blooshiez.github.io/",
+  base: "/TasoTsuboEn",
   trailingSlash: "always",
   integrations: [
     tailwind(
